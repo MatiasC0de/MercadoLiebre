0a29b168cb92b97bff8ab33631f9b158ded7a19a
Matias <matias@MBP-de-Matias.fibertel.com.ar>
2021-12-12 14:51:37 -0300

    Trabajo Finalizado sincronizado al repo

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000') );
+
+app.get('/', (req,res) => {
+    let htmlPath = path.resolve(__dirname, 'views/home.html');
+    res.sendFile(htmlPath);
+});
+
+app.get('/register', (req,res) => {
+    let htmlPath = path.resolve(__dirname, 'views/register.html');
+    res.sendFile(htmlPath);
+});
+
+app.get('/login', (req,res) => {
+    let htmlPath = path.resolve(__dirname, 'views/login.html');
+    res.sendFile(htmlPath);
+});
+
